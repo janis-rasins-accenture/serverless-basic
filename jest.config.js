b158a8f0f5688bb11384eb931c299619d9a7f244
Janis Rasins <extern.janis.rasins@vwfs.io>
2023-12-05 12:24:51 +0200

    Start of the 2nd session

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  clearMocks: false,
+  collectCoverage: true,
+  coverageDirectory: '../coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      lines: 85,
+    },
+  },
+  rootDir: 'src',
+};
